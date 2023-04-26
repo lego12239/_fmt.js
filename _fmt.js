bc2ac73346dd3e310618c338d3b99f6fddcd8fdb
Oleg Nemanov <lego12239@yandex.ru>
2023-04-26 18:39:47 +0300

    update copyright info

--- _fmt.js
+++ _fmt.js
@@ -1,7 +1,7 @@
 /*
  * Version 1.0
  *
- * Copyright 2021 Oleg Nemanov <lego12239@yandex.ru>
+ * Copyright 2021-2023 Oleg Nemanov <lego12239@yandex.ru>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
